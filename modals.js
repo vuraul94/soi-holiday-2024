739255dff5a9cec416763b62bf71c489acccdaf4
Raúl Venegas Ugalde <vuraul94@gmail.com>
2024-11-27 09:58:49 -0600

    some fixes

--- modals.js
+++ modals.js
@@ -23,8 +23,9 @@ function openModal1(content) {
         <div class="video">
           <iframe src="https://www.youtube.com/embed/zsLKnqm8GYA" title="Holiday Card - FKt240108" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
-        <p>This Chaunacops, or “walking” fish captured imaginations and garnered lots of attention. “The sea toad has modified fins that allow it to walk on the seafloor — partly a hunting strategy and partly because it’s more energy efficient than swimming.” Co-Chief Scientist Javier Sellanes explained. “As the first scientific report of a sea toad in the southeastern Pacific, the sea toad is likely a new species.”</p>
-    `);
+        <p>This Chaunacops, or “walking fish,” captured imaginations and drew lots of attention. This was the first scientific report of a sea toad in the southeastern Pacific, so likely a new species, according to co-chief scientist Javier Sellanes.</p>
+        <p class="note">Click expedition title for more info</p>
+        `);
   modal.open();
 }
 
@@ -39,7 +40,8 @@ function openModal2() {
           <iframe src="https://www.youtube.com/embed/WVoQMePYq4A" title="Holiday Card - FKt240224 Strawberry Squid" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <p>Finding light in the darkness is not impossible. In the deep Ocean, where the sun’s rays cannot reach, creatures like this strawberry squid seem to sparkle with their jewel-like luminescent photophores.</p>
-    `);
+        <p class="note">Click expedition title for more info</p>
+        `);
   modal.open();
 }
 
@@ -54,7 +56,8 @@ function openModal3(content) {
         <iframe src="https://www.youtube.com/embed/tYUSW1MCOZQ" title="Holiday Card - FKt240224 Instrument plunge" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <p>This automated mini-laboratory collects and processes seawater samples in situ at specified depths to study biogeochemical processes while accurately measuring trace amounts of oxygen in the water. This instrument will allow scientists to better to create better regional models of oxygen minimum zones as climate change causes them to expand.</p>
-    `);
+        <p class="note">Click expedition title for more info</p>
+        `);
   modal.open();
 }
 
@@ -69,7 +72,8 @@ function openModal4(content) {
           <iframe src="https://www.youtube.com/embed/v4yuy-1iu5A" title="Holiday Card - FKt240524 Munnopsid isopod" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <p>This deep-sea daddy long-legs, more accurately described as a munnopsid isopod, was observed during the <a href="https://www.youtube.com/hashtag/atacamatrenchancients" target="_blank" rel=”noopener noreferrer”>#AtacamaTrenchAncients</a> expedition off the coast of Chile. An international team was working to characterize the microbial and macrofaunal communities in the surrounding areas of the Atacama Trench.</p>
-    `);
+        <p class="note">Click expedition title for more info</p>
+        `);
   modal.open();
 }
 
@@ -83,8 +87,9 @@ function openModal5(content) {
         <div class="video">
           <iframe src="https://www.youtube.com/embed/SVO4j5ubq2o" title="Holiday Card - FKt240708 Corals" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
-        <p>Erin Easton, an expert in seafloor ecology, describes the area as “The most extensive marine animal forest seen on any of the expeditions to these ridges this year.” During dive 698, scientists observed this incredibly dense grouping of corals and sponges, and Easton suspects that the coral colonies are hundreds, or perhaps, thousands of years old; the sponges are ancient as well.</p>
-    `);
+        <p>Erin Easton, an expert in seafloor ecology, describes the area as, “The most extensive marine animal forest seen on any of the expeditions to these ridges this year.” During dive 698, scientists observed this incredibly dense grouping of corals and sponges, and Easton suspects that the coral colonies are hundreds, or perhaps, thousands of years old. The sponges are ancient as well.</p>
+        <p class="note">Click expedition title for more info</p>
+        `);
   modal.open();
 }
 
@@ -99,7 +104,8 @@ function openModal6(content) {
           <iframe src="https://www.youtube.com/embed/p0bNJV2WSlo" title="Holiday Card - FKt240802 Fire &amp; Ice" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <p>The #EruptionArchive scientists collected multiple types of core samples and painstakingly sifted through sediment and mud to document and study the impact of the 2008 Chaitén Volcano’s eruption on the marine environment. Their research could unlock deeper-time records of major volcanic eruptions in the region, allowing scientists to reconstruct volcanic behavior in Southern Chile over geologic time.</p>
-    `);
+        <p class="note">Click expedition title for more info</p>
+        `);
   modal.open();
 }
 
@@ -113,7 +119,8 @@ function openModal7(content) {
         <div class="video">
           <iframe src="https://www.youtube.com/embed/PPFOBrfia7E" title="Holiday Card - FKt241011 Onyx squid hatching" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
-        <p>This Gonatus onyx squid is not only carrying her egg sack, but she is also helping free the hatchlings! The black-eyed squid is one of the few that brood their eggs — most abandon them after laying them anchored to the seafloor. A female Gonatus onyx will carry her large egg mass for months, keeping it suspended from hooks on her arms. After laying the eggs she will go without feeding. By the time they hatch, she will be close to death. Researchers suspect the incubation time is 6 to 9 months, during which the female will pump water around the cluster (up to 3,000 eggs) as she swims, keeping them supplied with oxygen.</p>
-    `);
+        <p>This&nbsp;<em>Gonatus onyx</em>&nbsp;squid is not only carrying her egg sack, but she is also helping free the hatchlings! The black-eyed squid is one of the few that brood their eggs &mdash; most abandon them after laying them anchored to the seafloor. A female&nbsp;<em>Gonatus onyx</em>&nbsp;will carry her large egg mass for months, keeping it suspended from hooks on her arms. After laying the eggs she will go without feeding. By the time they hatch, she will be close to death. Researchers suspect the incubation time is 6 to 9 months, during which the female will pump water around the cluster (up to 3,000 eggs) as she swims, keeping them supplied with oxygen.</p>
+        <p class="note">Click expedition title for more info</p>
+      `);
   modal.open();
 }
