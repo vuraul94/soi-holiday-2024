0d0681ef6aed2c72690aaa0039d3ec41982e2a9c
Raúl Venegas Ugalde <vuraul94@gmail.com>
2024-12-04 15:27:28 -0600

    text fixes

--- modals.js
+++ modals.js
@@ -23,7 +23,7 @@ function openModal1(content) {
         <div class="video">
           <iframe src="https://www.youtube.com/embed/zsLKnqm8GYA" title="Holiday Card - FKt240108" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
-        <p>This Chaunacops, or “walking fish,” captured imaginations and drew lots of attention. This was the first scientific report of a sea toad in the southeastern Pacific, so likely a new species, according to co-chief scientist Javier Sellanes.</p>
+        <p>This Chaunacops, or “walking” fish captured imaginations and garnered lots of attention. “The sea toad has modified fins that allow it to walk on the seafloor — partly a hunting strategy and partly because it’s more energy efficient than swimming”, Co-Chief Scientist Javier Sellanes explained. “As the first scientific report of a sea toad in the southeastern Pacific, the sea toad is likely a new species.”</p>
         <p class="note">Click expedition title for more info</p>
         `);
   modal.open();
@@ -33,7 +33,7 @@ function openModal2() {
   modal.setContent(/*html*/ `
         <h2>
           <a href="https://schmidtocean.org/cruise/unexplored-seamounts-of-the-salas-y-gomez-ridge/" target="_blank" rel=”noopener noreferrer”>
-          Unexplored Seamounts of the Salas y Gomez Ridge
+          Unexplored Seamounts of the Salas y Gómez Ridge
           </a>
         </h2>
         <div class="video">
@@ -55,7 +55,7 @@ function openModal3(content) {
         <div class="video">
         <iframe src="https://www.youtube.com/embed/tYUSW1MCOZQ" title="Holiday Card - FKt240224 Instrument plunge" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
-        <p>This automated mini-laboratory collects and processes seawater samples in situ at specified depths to study biogeochemical processes while accurately measuring trace amounts of oxygen in the water. This instrument will allow scientists to better to create better regional models of oxygen minimum zones as climate change causes them to expand.</p>
+        <p>This automated mini-laboratory collects and processes seawater samples&nbsp;<em>in situ</em>&nbsp;at specified depths to study biogeochemical processes while accurately measuring trace amounts of oxygen in the water. This instrument will allow scientists to create better regional models of oxygen minimum zones as climate change causes them to expand.</p>
         <p class="note">Click expedition title for more info</p>
         `);
   modal.open();
@@ -71,7 +71,7 @@ function openModal4(content) {
         <div class="video">
           <iframe src="https://www.youtube.com/embed/v4yuy-1iu5A" title="Holiday Card - FKt240524 Munnopsid isopod" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
-        <p>This deep-sea daddy long-legs, more accurately described as a munnopsid isopod, was observed during the <a href="https://www.youtube.com/hashtag/atacamatrenchancients" target="_blank" rel=”noopener noreferrer”>#AtacamaTrenchAncients</a> expedition off the coast of Chile. An international team was working to characterize the microbial and macrofaunal communities in the surrounding areas of the Atacama Trench.</p>
+        <p>This deep-sea daddy long-legs, more accurately described as a Munnopsis isopod, was observed during the <a href="https://www.youtube.com/hashtag/atacamatrenchancients" target="_blank" rel=”noopener noreferrer”>#AtacamaTrenchAncients</a> expedition off the coast of Chile. An international team was working to characterize the microbial and macrofaunal communities in the surrounding areas of the Atacama Trench.</p>
         <p class="note">Click expedition title for more info</p>
         `);
   modal.open();
@@ -81,7 +81,7 @@ function openModal5(content) {
   modal.setContent(/*html*/ `
         <h2>
           <a href="https://schmidtocean.org/cruise/high-seas-and-seamounts/" target="_blank" rel=”noopener noreferrer”>
-          High Seas and Seamounts
+          High Seas and Seamounts of the Nazca Ridge
           </a>
         </h2>
         <div class="video">
@@ -97,7 +97,7 @@ function openModal6(content) {
   modal.setContent(/*html*/ `
         <h2>
           <a href="https://schmidtocean.org/cruise/fire-and-ice-volcanic-and-glacial-interactions/" target="_blank" rel=”noopener noreferrer”>
-            Fire and Ice Volcanic and Glacial Interactions
+            Fire and Ice: Volcanic and Glacial Interactions 
           </a>
         </h2>
         <div class="video">
@@ -119,7 +119,7 @@ function openModal7(content) {
         <div class="video">
           <iframe src="https://www.youtube.com/embed/PPFOBrfia7E" title="Holiday Card - FKt241011 Onyx squid hatching" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
-        <p>This&nbsp;<em>Gonatus onyx</em>&nbsp;squid is not only carrying her egg sack, but she is also helping free the hatchlings! The black-eyed squid is one of the few that brood their eggs &mdash; most abandon them after laying them anchored to the seafloor. A female&nbsp;<em>Gonatus onyx</em>&nbsp;will carry her large egg mass for months, keeping it suspended from hooks on her arms. After laying the eggs she will go without feeding. By the time they hatch, she will be close to death. Researchers suspect the incubation time is 6 to 9 months, during which the female will pump water around the cluster (up to 3,000 eggs) as she swims, keeping them supplied with oxygen.</p>
+        <p>Some deep-sea worms are bioluminescent, but this sassy sparkler has protein structures in the bristles that make them iridescent. Polychaetes play a vital role throughout our global Ocean, and seeing this unusual animal make headlines brought smiles to the Schmidt Ocean Institute team.</p>
         <p class="note">Click expedition title for more info</p>
       `);
   modal.open();
